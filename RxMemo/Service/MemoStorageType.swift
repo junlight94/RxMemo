ebe9267532fa22c340f7f8fa2546a1e93f32f37d
Junyoung <junlight94@gmail.com>
2023-01-09 12:22:29 +0900

    Step4

---
+++ RxMemo/Service/MemoStorageType.swift
@@ -0,0 +1,23 @@
+//
+//  MemoStorageType.swift
+//  RxMemo
+//
+//  Created by Junyoung on 2023/01/09.
+//
+
+import Foundation
+import RxSwift
+
+protocol MemoStorageType {
+    @discardableResult
+    func create(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func memoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
