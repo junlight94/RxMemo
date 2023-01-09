613b80c388fb1b2a8ef316f9092da403fadcb08a
Junyoung <junlight94@gmail.com>
2023-01-09 12:36:06 +0900

    step6

---
+++ RxMemo/ViewController/MemoListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemoListViewController.swift
+//  RxMemo
+//
+//  Created by Junyoung on 2023/01/09.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
