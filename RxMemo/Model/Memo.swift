1918e1495091238c4cc835af25b94e3de615631f
Junyoung <junlight94@gmail.com>
2023-01-10 20:01:50 +0900

    Step 22

--- RxMemo/Model/Memo.swift
+++ RxMemo/Model/Memo.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import RxDataSources
+import RxCoreData
+import CoreData
 
 // Equatable 프로토콜 채용
 struct Memo: Equatable, IdentifiableType {
@@ -26,5 +28,33 @@ struct Memo: Equatable, IdentifiableType {
     init(original: Memo, updateContent: String) {
         self = original
         self.content = updateContent
+    }
+}
+
+extension Memo: Persistable {
+    static var entityName: String {
+        return "Memo"
+    }
+    
+    static var primaryAttributeName: String {
+        return "identity"
+    }
+    
+    init(entity: NSManagedObject) {
+        content = entity.value(forKey: "content") as! String
+        insertDate = entity.value(forKey: "insertDate") as! Date
+        identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    func update(_ entity: NSManagedObject) {
+        entity.setValue(content, forKey: "content")
+        entity.setValue(insertDate, forKey: "insertDate")
+        entity.setValue("\(insertDate.timeIntervalSinceReferenceDate)", forKey: "identity")
+        
+        do {
+            try entity.managedObjectContext?.save()
+        } catch {
+            print(error)
+        }
     }
 }
